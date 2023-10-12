4a44ca0e208818e3319b0117cb2b6766846f57b4
MYETRO <3490118+MYETRO@users.noreply.github.com>
2023-10-12 21:09:50 +0100

    async TrGetFile

--- server/RdtClient.Service/Services/Downloaders/SymlinkDownloader.cs
+++ server/RdtClient.Service/Services/Downloaders/SymlinkDownloader.cs
@@ -122,35 +122,95 @@ public class SymlinkDownloader : IDownloader
         }
     }
 
-        private static FileInfo? TryGetFile(string filePath)
-        {
-            var file = new FileInfo(filePath);
+        // private static FileInfo? TryGetFile(string filePath)
+        // {
+        //     var file = new FileInfo(filePath);
             
-            // Check if the file exists at the provided path
-            if (file.Exists)
+        //     // Check if the file exists at the provided path
+        //     if (file.Exists)
+        //     {
+        //         return file; // Return the file if it exists
+        //         // logger.Error($"file found {file}");
+        //     }
+
+        //     // If the file is not found at the specified path, you can get the directory
+        //     var dirInfo = file.Directory;
+
+        //     if (dirInfo != null)
+        //     {
+        //         var filesInDirectory = dirInfo.EnumerateFiles();
+        //         var fileInDirectory = filesInDirectory.FirstOrDefault(f => f.Name == file.Name);
+
+        //         if (fileInDirectory != null)
+        //         {
+        //             return fileInDirectory; // Return the file found in the directory
+        //         }
+        //     }
+
+        //     // If the file is not found in the specified directory, return null.
+        //     return null;
+        // }
+
+
+        // static void Main(string[] args)
+        private static FileInfo? TryGetFile(string Name, string filePath)
+        {
+            // The directory that contains the subdirectories and files
+            string dir = @"C:\Users\test\Downloads";
+
+            // The file name to search for
+            string fileName = "79031V0 - Copy.jpg";
+
+            // Get all the subdirectories in the directory
+            string[] subdirs = Directory.GetDirectories(dir);
+
+            // Loop through each subdirectory
+            foreach (string subdir in subdirs)
             {
-                return file; // Return the file if it exists
-                // logger.Error($"file found {file}");
-            }
+                // Get the name of the subdirectory
+                string folderName = Path.GetFileName(subdir);
 
-            // If the file is not found at the specified path, you can get the directory
-            var dirInfo = file.Directory;
+                // Search for the file in the subdirectory
+                string[] files = Directory.GetFiles(subdir, fileName);
 
-            if (dirInfo != null)
-            {
-                var filesInDirectory = dirInfo.EnumerateFiles();
-                var fileInDirectory = filesInDirectory.FirstOrDefault(f => f.Name == file.Name);
-
-                if (fileInDirectory != null)
+                // Check if the file was found
+                if (files.Length > 0)
                 {
-                    return fileInDirectory; // Return the file found in the directory
+                    // Get the full path of the file
+                    string filePath = files[0];
+
+                    // Print a message with the folder name and file path
+                    'Console.WriteLine("The file {0} was found in folder {1} with path {2}", fileName, folderName, filePath);
+                    return file; // File found, return it
                 }
             }
-
-            // If the file is not found in the specified directory, return null.
-            return null;
         }
 
+
+        // // static void Main(string[] args)
+        // private static FileInfo? TryGetFile(string Name, string filePath)
+        // {
+        //     // The file path of the file to be renamed
+        //     string filePath = @"C:\Users\gbvantole\Downloads\DA003004V2 - Copy\DA003004V2 - Copy.jpg";
+
+        //     // The new name of the file
+        //     string newName = "DA003004V2 vs renamed.jpg";
+
+        //     // Get the folder name of the file
+        //     string folderName = Path.GetDirectoryName(filePath);
+        //     //log.WriteLine("Folder name from filePath" {folderName});
+        //     // Get the file object
+        //     FileInfo file = new FileInfo(filePath);
+
+        //     // Rename the file by moving it to the same folder with a new name
+        //     // string newFile = Path.Combine(folderName, newName);
+        //     // file.MoveTo(newFile);
+
+        //     // Print a message when done
+        //     Console.WriteLine("The file has been renamed in {0}", folderName);
+        // }
+
+        //          WORKING -------------------------------------- >
         // private static FileInfo? TryGetFile(string Name)
         // {
         //     var dirInfo = new DirectoryInfo(Settings.Get.DownloadClient.RcloneMountPath);
@@ -176,6 +236,40 @@ public class SymlinkDownloader : IDownloader
         //         // If the code reaches here, it means the file was not found in subdirectories.
         //         // Continue the while loop to start the search again.
         //     }
+
+    private static async Task<FileInfo?> TryGetFileAsync(string Name, string filePath)
+    {
+        var dirInfo = new DirectoryInfo(filePath);
+
+        while (true)
+        {
+            var sortedDirectories = await Task.Run(() =>
+            {
+                return dirInfo.GetDirectories()
+                    .OrderByDescending(d => d.CreationTime)
+                    .ToList();
+            });
+
+            foreach (var dir in sortedDirectories)
+            {
+                var files = dir.EnumerateFiles();
+                var file = files.FirstOrDefault(f => f.Name == Name);
+                if (file != null)
+                {
+                    return file; // File found, return it
+                }
+            }
+            // If the code reaches here, it means the file was not found in subdirectories.
+            // Continue the while loop to start the search again.
+        }
+        // If the while loop exits, it means the file was not found in subdirectories.
+        // Return null.
+        return null;
+    }
+
+
+
+
 
 
         //     // // Get the subdirectories sorted by creation date in descending order
